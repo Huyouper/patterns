01a3f1a7d9dbe1e21b4adfb56a517ef1113afce9
Huyouper <85759954+Huyouper@users.noreply.github.com>
2022-03-29 17:49:15 +0600

    Delete Shape.java

--- Bakytov Erjan LAB 3/src/com/company/Shape.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-abstract class Shape {
-    protected Double perimeter;
-    protected Double area;
-    public abstract Double calculatePerimeter();
-    public abstract Double calculateArea();
-}
